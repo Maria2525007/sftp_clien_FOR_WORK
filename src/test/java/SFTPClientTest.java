8965c2c4d8838e6567d50bd2b3f49b759934e34b
Makhmudova Maria <maria-06112004@mail.ru>
2025-01-30 10:03:36 +0300

    writing tests

---
+++ src/test/java/SFTPClientTest.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class SFTPClientTest {
+
+    @Test
+    public void testSuccessfulConnection() {
+
+    }
+
+    @Test
+    public void testAddNewDomainIpPair() {
+
+    }
+
+
+}
